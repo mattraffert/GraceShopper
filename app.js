722bfc9b741a013b47837ac5680a63c4338df6b2
Matthew Rafferty <mattraffert@gmail.com>
2023-03-30 18:48:15 -0400

    Changes

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-require("dotenv").config()
-const express = require("express")
-const app = express()
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const client = require('./db/client');
+require('dotenv').config();
 
-// Setup your Middleware and API Router here
+const server = express();
+const { PORT = 3000 } = process.env;
+const { apiRouter } = require('./api/index');
 
-module.exports = app;
+server.use(cors());
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(morgan('dev'));
+server.use('/api', apiRouter);
+
+server.use((req, res, next) => {
+	res.sendStatus(404);
+});
+
+server.use((error, req, res, next) => {
+	console.log('Server Log', error);
+	res.send(error);
+});
+
+server.listen(PORT, () => {
+	client.connect();
+	console.log(`Listening on port ${PORT}`);
+});
